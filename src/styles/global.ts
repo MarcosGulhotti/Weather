64a50d9a0efe921fc111bade4131c64c925147a9
MarcosGulhotti <marcosmvg77@gmail.com>
2022-10-05 18:09:17 -0300

    feat: adding colors and fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    :root{}
+    :root{
+        --primary: #269AF2;
+        --secondary: #323544;
+        --background-color: #1E202B;
+        --dark-gray: #262936;
+        --darker-gray: #222531;
+        --text-color: #AEAEAE;
+        --white: #FFFFFF;
+        --font-roboto: 'Roboto', sans-serif;;
+        --font-roboto-mono: 'Roboto Mono', monospace;;
+    }
 `;
 
 export default GlobalStyles;
